53b37258807ae5e57082cdec4e3dceb5c42e335d
Aleksander558833 <alexander_2424@mail.ru>
2024-07-14 00:54:22 +0900

    news

--- news/NewsPaper/news/templatetags/custom_filters.py
+++ news/NewsPaper/news/templatetags/custom_filters.py
@@ -2,11 +2,11 @@ from django import template
 
 register = template.Library()
 
-censor_list = ['Макрон', 'эксперименте']
+censor_list = ['Франции', 'эксперименте']
 
 @register.filter()
 def censor(value):
     for word in censor_list:
         value = value.replace(word[1:], '*' * len(word[1:]))
-        return value
+    return value
 
